8011bcadbeefcad5265b43558e9ee772efe2cae1
crash08 <zavarnov_yura@mail.ru>
2022-02-12 22:19:37 +0300

    Добавлены 2 слайдера в секции hero и about

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+})
