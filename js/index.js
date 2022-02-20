47a80057e1effc66431a8a3d1dc312eaf65ff45c
crash08 <zavarnov_yura@mail.ru>
2022-02-20 20:08:49 +0300

    Реализованы форма обратной связи и hover эффекты

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
 
+  //Инициализация свайпера в секции hero
   const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
@@ -23,12 +24,11 @@ window.addEventListener('DOMContentLoaded', function () {
   });
 
 
-
+  //Инициализация свайпера в секции about
   const swiper2 = new Swiper('.swiper-2', {
-    // Optional parameters
+
     loop: true,
 
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
@@ -47,6 +47,7 @@ window.addEventListener('DOMContentLoaded', function () {
   });
 
 
+  //Назначение действий кнопок свайпера на свои классы
   const swiperPrev = document.getElementById('swiperPrev')
   const swiperNext = document.getElementById('swiperNext')
 
@@ -68,4 +69,51 @@ window.addEventListener('DOMContentLoaded', function () {
   function windowScroll() {
     header.classList.toggle("scroll", header.scrollTop > 10 || document.documentElement.scrollTop > 10);
   }
+
+
+  //Реализация модального окна "Обратный звонок"
+  var headerCall = document.querySelector('#headerCall');
+  var callbackPopupWrap = document.querySelector('#callbackPopupWrap');
+  var callbackPopupForm = document.querySelector('#callbackPopupForm');
+  var callbackPopupResult = document.querySelector('#callbackPopupResult');
+  var callbackPopupCloseResult = document.querySelector('#callbackPopupCloseResult');
+  var callbackPopupClose = document.querySelector('#callbackPopupClose');
+
+  headerCall.addEventListener('click', function () {
+    callbackPopupWrap.style.display = 'flex';
+  })
+  callbackPopupClose.addEventListener('click', function () {
+    callbackPopupWrap.style.display = 'none';
+  })
+  callbackPopupCloseResult.addEventListener('click', function () {
+    callbackPopupWrap.style.display = 'none';
+  })
+
+
+  //Отправка формы и проверка ответа сервера
+  var form = document.querySelector('#form')
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var data = new FormData(this) // Сборка формы
+    var url = 'https://jsonplaceholder.typicode.com/posts'
+    fetch(url, {
+      method: 'post',
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+      },
+      body: data // Отправка самой формы
+    })
+      .then(response => response.json())
+      .then((json) => { // Ответ
+        if (json.id === 101) { // Для примера проверка пройдена если id === 101
+          // Если проверка пройдена
+          callbackPopupForm.style.display = 'none';
+          callbackPopupResult.style.display = 'flex';
+        }
+        // Дебаг узнать что прошла форма
+        //console.log(json)
+      })
+    //.catch(err => console.log(err));
+  })
 })
